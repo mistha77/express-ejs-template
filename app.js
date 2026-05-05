3f62e5947435322a1c2662bf77b50317676be083
Mishu <your-email@example.com>
2026-05-05 17:07:40 +0530

    first commit - express ejs project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set("view engine","ejs");
+
+app.use(express.urlencoded({extended:true}));
+app.get("/",(req, res)=>{
+    res.render("index",{title:"express Templates",
+        message: "welcome to express templates !",
+        greeting:"",
+    });
+});
+app.post("/",(req, res)=>{
+    const{name} = req.body;
+    const greeting = `hello,${name}!`
+    res.render("index",{title:"Express Templates",
+        message: "welcome to express templates !",
+        greeting,
+    })
+});
+app.listen(port,()=>{
+    console.log(`server is running on port:${port}`);
+});
